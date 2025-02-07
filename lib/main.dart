efb2fd92ef18065f1c917706acd7cf43251b60b2
SUGGESTIED ✨ <74950245+suggestied@users.noreply.github.com>
2025-02-07 04:23:47 +0100

    Load environment variables from .env file during initialization

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,8 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-
+  // load dotenv
+  await dotenv.load(fileName: ".env");
 
   // Initialize Supabase
   await Supabase.initialize(
