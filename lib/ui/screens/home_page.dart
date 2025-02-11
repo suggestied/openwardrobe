21de205c2fddbea84e8c462329dcafc22e24ccb9
SUGGESTIED ✨ <74950245+suggestied@users.noreply.github.com>
2025-02-11 10:03:37 +0100

    Remove unused Supabase migration and .gitignore; enhance wardrobe item fetching and syncing logic

--- lib/ui/screens/home_page.dart
+++ lib/ui/screens/home_page.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:go_router/go_router.dart';
+// import  waardrobe service from this project
+import 'package:openwardrobe/services/wardrobe_service.dart';
+import 'package:openwardrobe/repositories/wardrobe_repository.dart';
+import 'package:openwardrobe/models/wardrobe_item.dart';
+
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+  Future<List<WardrobeItem>> _getClothes() async {
+    final wardrobeService = WardrobeService(WardrobeRepository());
+    return await wardrobeService.getWardrobeItems();
+  }
 
   @override
   Widget build(BuildContext context) {
     final supabase = Supabase.instance.client;
+
+
+
 
     Future<void> _signOut() async {
       await supabase.auth.signOut();
@@ -21,8 +33,31 @@ class HomeScreen extends StatelessWidget {
           IconButton(icon: const Icon(Icons.logout), onPressed: _signOut),
         ],
       ),
-      body: const Center(
-        child: Text('Welcome to OpenWardrobe! You are logged in.'),
+      body: Center(
+        // A dashboard with all the wardrobe items
+        child: FutureBuilder<List<WardrobeItem>>(
+          future: _getClothes(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else if (snapshot.hasData) {
+              final clothes = snapshot.data;
+              return ListView.builder(
+                itemCount: clothes?.length ?? 0,
+                itemBuilder: (context, index) {
+                  final item = clothes![index];
+                  return ListTile(
+                    title: Text(item.name),
+                  );
+                },
+              );
+            } else {
+              return const Text('No data');
+            }
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => context.go('/wardrobe'),
