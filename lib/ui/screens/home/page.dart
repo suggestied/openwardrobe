de9103dca5f8b1c5f959a6084e0f0f38b122027f
Luc Lammers <luclammers06@gmail.com>
2025-02-14 11:43:56 +0100

    Update Podfile.lock and pubspec.yaml for dependency version changes; refactor HomeScreen layout

--- lib/ui/screens/home/page.dart
+++ lib/ui/screens/home/page.dart
@@ -9,102 +9,86 @@ import 'package:openwardrobe/ui/widgets/wardrobe_item/wardrobe_item_component.da
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
-  
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Welcome, username and profile picture
         title: const Text('Home'),
-
       ),
       body: Align(
-  alignment: Alignment.topCenter, // Houdt de items bovenaan en gecentreerd horizontaal
-  child: Column(
-    // DashboardLink
-    children: [
-      // Max width for row
-      
-    ConstrainedBox(constraints: BoxConstraints(maxWidth: 500),
-    child: 
-      // Make a row
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          DashboardLink(
-        text: 'Upload Items',
-        icon: Icons.add,
-        color: Colors.blue,
-        onTap: () {
-        },
-      ),
-      DashboardLink(
-        text: 'Create Outfit',
-        color: Colors.red,
-        icon: Icons.list,
-        onTap: () {
-        },
-      ),
-      DashboardLink(
-        text: 'Schedule Outfit',
-        color: Colors.green,
-        icon: Icons.calendar_today,
-        onTap: () {
-        },
-      ),
-      // View my stats
-      DashboardLink(
-        text: 'View Stats',
-        color: Colors.purple,
-        icon: Icons.bar_chart,
-        onTap: () {
-        },
-      ),
-        ],
-      ),
-    ),  
-    
-    
-    const SizedBox(height: 20),
-    Expanded(
-      child: ConstrainedBox(
-        constraints: const BoxConstraints(maxWidth: 500),
-        child: FutureBuilder<List<WardrobeItem>>(
-          future: GetIt.instance<WardrobeItemService>().getAll(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            }
+          alignment: Alignment
+              .topCenter,
+          child: Column(
+            children: [
+              ConstrainedBox(
+                constraints: BoxConstraints(maxWidth: 500),
+                child:
+                    // Make a row
+                    Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    DashboardLink(
+                      text: 'Upload Items',
+                      color: Colors.blue,
+                      icon: Icons.add,
+                      onTap: () {},
+                    ),
+                    DashboardLink(
+                      text: 'Create Outfit',
+                      color: Colors.red,
+                      icon: Icons.list,
+                      onTap: () {},
+                    ),
+                    DashboardLink(
+                      text: 'Schedule Outfit',
+                      color: Colors.green,
+                      icon: Icons.calendar_today,
+                      onTap: () {},
+                    ),
+                    // View my stats
+                    DashboardLink(
+                      text: 'View Stats',
+                      color: Colors.purple,
+                      icon: Icons.bar_chart,
+                      onTap: () {},
+                    ),
+                  ],
+                ),
+              ),
 
-            if (snapshot.hasError) {
-              return Center(child: Text('Error: ${snapshot.error}'));
-            }
+              const SizedBox(height: 20),
+              Expanded(
+                child: ConstrainedBox(
+                  constraints: const BoxConstraints(maxWidth: 500),
+                  child: FutureBuilder<List<WardrobeItem>>(
+                    future: GetIt.instance<WardrobeItemService>().getAll(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(child: CircularProgressIndicator());
+                      }
 
-            final items = snapshot.data ?? [];
+                      if (snapshot.hasError) {
+                        return Center(child: Text('Error: ${snapshot.error}'));
+                      }
 
-            if (items.isEmpty) {
-              return const Center(child: Text('No items found'));
-            }
+                      final items = snapshot.data ?? [];
 
-            return ListView.builder(
-              itemCount: items.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text("test")
-                );
-              },
-            );
-          },
-        ),
-      ),
-    )
-    ],
+                      if (items.isEmpty) {
+                        return const Center(child: Text('No items found'));
+                      }
 
-  )
-  ),
-
-
+                      return ListView.builder(
+                        itemCount: items.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(title: Text("test"));
+                        },
+                      );
+                    },
+                  ),
+                ),
+              )
+            ],
+          )),
     );
   }
 }
